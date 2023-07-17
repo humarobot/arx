88463d998cf6651551c84e86e11f3b93b09ca6d5
liqikai <li_qi_kai@qq.com>
2023-07-17 13:49:44 +0800

    add joint torque control

---
+++ ultron/src/main_impedence.cpp
@@ -0,0 +1,181 @@
+
+#include "pinocchio/algorithm/crba.hpp"
+#include "pinocchio/algorithm/frames.hpp"
+#include "pinocchio/algorithm/jacobian.hpp"
+#include "pinocchio/algorithm/joint-configuration.hpp"
+#include "pinocchio/algorithm/kinematics.hpp"
+#include "pinocchio/algorithm/rnea.hpp"
+#include "pinocchio/multibody/fwd.hpp"
+#include "pinocchio/parsers/urdf.hpp"
+#include "ros/ros.h"
+#include "sensor_msgs/JointState.h"
+#include "std_msgs/Float64.h"
+#include "std_msgs/Float64MultiArray.h"
+
+/** Dynamic-size vector type. */
+using vector_t = Eigen::Matrix<double, Eigen::Dynamic, 1>;
+
+// Define a robot arm joint struct to store joint information
+struct Joint {
+  std::string name;
+  double position;
+  double velocity;
+  double acceleration;
+  double effort;
+  // Define a joint constructor
+  Joint() {
+    name = "";
+    position = 0.0;
+    velocity = 0.0;
+    acceleration = 0.0;
+    effort = 0.0;
+  }
+};
+// Define a robot arm struct to store robot arm information
+struct RobotArm {
+  std::vector<Joint> joints;
+  Eigen::Vector3d position;
+  Eigen::Quaterniond orientation;
+  Eigen::Matrix3d rotation;
+  // Define a robot arm constructor
+  RobotArm() {
+    // Add 6 joints to the robot arm
+    joints.resize(6);
+    position = Eigen::Vector3d::Zero();
+    orientation = Eigen::Quaterniond::Identity();
+    rotation = Eigen::Matrix3d::Identity();
+  }
+};
+
+RobotArm ultron_arm_now, ultron_arm_last;
+// Define update rate
+int update_rate = 200;
+double update_period = 1.0 / update_rate;
+vector_t q(6), v(6), a(6);
+
+void jointStateCallback(const sensor_msgs::JointState::ConstPtr &msg) {
+  // Clear the ultron_arm_now
+  ultron_arm_now.joints.clear();
+  // Push back the joint information to ultron_arm_now
+  for (int i = 0; i < msg->name.size(); i++) {
+    Joint joint;
+    joint.name = msg->name[i];
+    joint.position = msg->position[i];
+    joint.velocity = msg->velocity[i];
+    joint.effort = msg->effort[i];
+    ultron_arm_now.joints.push_back(joint);
+  }
+  // Compute joint acceleration and using low pass filter to smooth the
+  // acceleration
+  for (int i = 0; i < ultron_arm_now.joints.size(); i++) {
+    ultron_arm_now.joints[i].acceleration =
+        (ultron_arm_now.joints[i].velocity -
+         ultron_arm_last.joints[i].velocity) /
+        update_period;
+    ultron_arm_now.joints[i].acceleration =
+        0.05 * ultron_arm_now.joints[i].acceleration +
+        0.95 * ultron_arm_last.joints[i].acceleration;
+  }
+  // Deep copy the ultron_arm_now to ultron_arm_last
+  ultron_arm_last = ultron_arm_now;
+  // Store joints info to q,v,a
+  q.resize(ultron_arm_now.joints.size());
+  v.resize(ultron_arm_now.joints.size());
+  a.resize(ultron_arm_now.joints.size());
+  for (int i = 0; i < ultron_arm_now.joints.size(); i++) {
+    q(i) = ultron_arm_now.joints[i].position;
+    v(i) = ultron_arm_now.joints[i].velocity;
+    a(i) = ultron_arm_now.joints[i].acceleration;
+  }
+}
+
+int main(int argc, char **argv) {
+  // Init ros node
+  ros::init(argc, argv, "ultron");
+  ros::NodeHandle nh;
+  ros::Rate loop_rate(200);
+  // Init a joint state topic subscriber
+  ros::Subscriber joint_state_sub =
+      nh.subscribe("ultron/joint_states", 10, jointStateCallback);
+  // Init joints effort topic publisher
+  ros::Publisher joint1_pub = nh.advertise<std_msgs::Float64>(
+      "/ultron/joint1_torque_controller/command", 1);
+  ros::Publisher joint2_pub = nh.advertise<std_msgs::Float64>(
+      "/ultron/joint2_torque_controller/command", 1);
+  ros::Publisher joint3_pub = nh.advertise<std_msgs::Float64>(
+      "/ultron/joint3_torque_controller/command", 1);
+  ros::Publisher joint4_pub = nh.advertise<std_msgs::Float64>(
+      "/ultron/joint4_torque_controller/command", 1);
+  ros::Publisher joint5_pub = nh.advertise<std_msgs::Float64>(
+      "/ultron/joint5_torque_controller/command", 1);
+  ros::Publisher joint6_pub = nh.advertise<std_msgs::Float64>(
+      "/ultron/joint6_torque_controller/command", 1);
+  // Init a joints acceleration topic publisher
+  ros::Publisher joint_acceleration_pub =
+      nh.advertise<std_msgs::Float64MultiArray>(
+          "ultron/joint_states_acceleration", 10);
+
+  using namespace pinocchio;
+  const std::string urdf_filename =
+      "/home/lqk/Ultron-motor/src/ultron/urdf/ultron.urdf";
+  // Load the urdf model
+  Model model;
+  pinocchio::urdf::buildModel(urdf_filename, model);
+  std::cout << "model name: " << model.name << std::endl;
+  // Create data required by the algorithms
+  Data data(model);
+  const int JOINT_ID = 6;
+  vector_t x_ref(6);
+  x_ref << 0.3, 0.0, 0.3, 0.0, 0.0, 0.0;
+  vector_t x(6);
+
+  while (ros::ok()) {
+    pinocchio::forwardKinematics(model, data, q, v);
+    pinocchio::computeJointJacobians(model, data);
+    pinocchio::updateFramePlacements(model, data);
+    pinocchio::crba(model, data, q);
+    data.M.triangularView<Eigen::StrictlyLower>() =
+        data.M.transpose().triangularView<Eigen::StrictlyLower>();
+    pinocchio::nonLinearEffects(model, data, q, v);
+    Eigen::Matrix<double, 6, 6> jac;
+    pinocchio::getJointJacobian(model, data, JOINT_ID, pinocchio::WORLD, jac);
+    // Compute torques
+    // x << data.oMi[JOINT_ID].translation();
+    // data.oMi[JOINT_ID].rotation() to euler angle
+    // x << data.oMi[JOINT_ID].rotation().eulerAngles(0,1,2);
+
+    // Eigen::VectorXd tau = data.nle - jac.transpose()*();
+    Eigen::VectorXd tau = data.nle;
+    // Publish the joints effort
+    std_msgs::Float64 joint1_msg;
+    joint1_msg.data = tau(0);
+    joint1_pub.publish(joint1_msg);
+    std_msgs::Float64 joint2_msg;
+    joint2_msg.data = tau(1);
+    joint2_pub.publish(joint2_msg);
+    std_msgs::Float64 joint3_msg;
+    joint3_msg.data = tau(2);
+    joint3_pub.publish(joint3_msg);
+    std_msgs::Float64 joint4_msg;
+    joint4_msg.data = tau(3);
+    joint4_pub.publish(joint4_msg);
+    std_msgs::Float64 joint5_msg;
+    joint5_msg.data = tau(4);
+    joint5_pub.publish(joint5_msg);
+    std_msgs::Float64 joint6_msg;
+    joint6_msg.data = tau(5);
+    joint6_pub.publish(joint6_msg);
+
+    // Publish the joints acceleration
+    std_msgs::Float64MultiArray joint_acceleration_msg;
+    for (int i = 0; i < 6; i++) {
+      joint_acceleration_msg.data.push_back(
+          ultron_arm_now.joints[i].acceleration);
+    }
+    joint_acceleration_pub.publish(joint_acceleration_msg);
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
+
+  return 0;
+}
