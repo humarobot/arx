34336d02cb4e1108b1749a9bf4c42ff5b417a1a7
liqikai <li_qi_kai@qq.com>
2023-10-25 14:16:35 +0800

    add: ultron pos package to do robot arm smooth position control

---
+++ ultron_pos/src/main.cpp
@@ -0,0 +1,19 @@
+#include "pinocchio/algorithm/crba.hpp"
+#include "pinocchio/algorithm/frames.hpp"
+#include "pinocchio/algorithm/jacobian.hpp"
+#include "pinocchio/algorithm/joint-configuration.hpp"
+#include "pinocchio/algorithm/kinematics.hpp"
+#include "pinocchio/algorithm/rnea.hpp"
+#include "pinocchio/multibody/fwd.hpp"
+#include "pinocchio/parsers/urdf.hpp"
+#include "pinocchio/spatial/explog.hpp"
+#include "ros/ros.h"
+#include "sensor_msgs/JointState.h"
+#include "std_msgs/Float64.h"
+#include "std_msgs/Float64MultiArray.h"
+#include "geometry_msgs/Pose.h"
+#include "App/arm_control.h"
+
+int main(int argc, char **argv) {
+  return 0;
+}
